fd4943bfce15d9e2167bd4463c3e4f09669084c9
Scott Steadman <scott.steadman@geni.com>
2025-05-27 19:09:25 -0700

    update test_my_app.sh
      trying to get rails-8.0.x branch to build

--- test_my_app.sh
+++ test_my_app.sh
@@ -12,6 +12,8 @@ bundle config --local clean true
 bundle config --local path vendor/bundle
 bundle config --local without vscode
 
+rm Gemfile.lock
 bundle install
+
 bundle exec rails db:create db:migrate
 bundle exec rails test
