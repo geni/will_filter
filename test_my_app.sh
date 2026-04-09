087b83d4a2010668797ec082750ac8fa1ff8c584
Scott Steadman <scott.steadman@geni.com>
2026-04-09 09:30:17 -0700

    update test_my_app.sh
      The stuff I removed is now handled by jenkins.

--- test_my_app.sh
+++ test_my_app.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 
-unset RBENV_VERSION
-export RBENV_ROOT=/opt/rbenv
-export PATH=${RBENV_ROOT}/shims:${PATH}:${RBENV_ROOT}/bin
-
-git gc
-
-bundle config --local build.pg "--with-pg-config=/usr/pgsql-10/bin/pg_config"
 bundle config --local build.sqlite3 "--enable-system-libraries"
 bundle config --local clean true
 bundle config --local path vendor/bundle
 bundle config --local without vscode
 
-rm Gemfile.lock
 bundle install
 
-#bundle exec rails db:create db:migrate
+rm db/test.sqlite3
 bundle exec rake test
