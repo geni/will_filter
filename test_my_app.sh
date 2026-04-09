8b5797925fca36fb397552ff0d6b5f889a57bbbb
Scott Steadman <scott.steadman@geni.com>
2026-04-09 09:58:02 -0700

    update test_my_app.sh

--- test_my_app.sh
+++ test_my_app.sh
@@ -5,6 +5,9 @@ bundle config --local clean true
 bundle config --local path vendor/bundle
 bundle config --local without vscode
 
+# The bundler version can change between branches
+rm -f Gemfile.lock
+
 bundle install
 
 bundle exec rails test
